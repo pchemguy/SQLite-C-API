292144d017ce174e6dffc98e321d20aa1e24a616
PChemGuy <39730837+pchemguy@users.noreply.github.com>
2021-10-07 22:01:49 +0300

    Scripts extracting SQLite API

---
+++ Library/SQLiteCAPI/Scripts/sqlite3.h.API.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+#
+set -euo pipefail
+IFS=$'\n\t'
+
+cleanup_EXIT() { 
+  echo "EXIT clean up: $?" 
+}
+trap cleanup_EXIT EXIT
+
+cleanup_TERM() {
+  echo "TERM clean up: $?"
+}
+trap cleanup_TERM TERM
+
+cleanup_ERR() {
+  echo "ERR clean up: $?"
+}
+trap cleanup_ERR ERR
+
+
+
+main() {
+(
+  pcre2grep -M '^SQLITE_API[^;]*\);' "./sqlite/build/sqlite3.h"
+  return 0
+) >sqlite3.h.API
+}
+
+
+main "$@"
